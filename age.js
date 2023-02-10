8066d50287492a1eac3d80264b420b005049c7c4
naibaipor <42374189+naibaipor@users.noreply.github.com>
2023-02-10 13:03:15 +0700

    sdsds
    ssdsd

---
+++ age.js
@@ -0,0 +1,60 @@
+document.getElementById("demo").innerHTML = getAge('08/28/1962') ;
+
+function getAge(dateString) {
+  var now = new Date();
+  var today = new Date(now.getYear(),now.getMonth(),now.getDate());
+
+  var yearNow = now.getYear();
+  var monthNow = now.getMonth();
+  var dateNow = now.getDate();
+
+  var dob = new Date(dateString.substring(6,10),
+                     dateString.substring(0,2)-1,                   
+                     dateString.substring(3,5)                  
+                     );
+
+  var yearDob = dob.getYear();
+  var monthDob = dob.getMonth();
+  var dateDob = dob.getDate();
+  var age = {};
+  var ageString = "";
+  var yearString = "";
+  var monthString = "";
+  var dayString = "";
+
+
+  yearAge = yearNow - yearDob;
+
+  if (monthNow >= monthDob)
+    var monthAge = monthNow - monthDob;
+  else {
+    yearAge--;
+    var monthAge = 12 + monthNow -monthDob;
+  }
+
+  if (dateNow >= dateDob)
+    var dateAge = dateNow - dateDob;
+  else {
+    monthAge--;
+    var dateAge = 31 + dateNow - dateDob;
+
+    if (monthAge < 0) {
+      monthAge = 11;
+      yearAge--;
+    }
+  }
+
+  age = {
+      years: yearAge,
+      months: monthAge,
+      days: dateAge
+      };
+
+  yearString = " ปี";
+  monthString = " เดือน";
+  dayString = " วัน";
+
+  ageString = age.years + yearString + " " + age.months + monthString + " " + age.days + dayString ;
+
+  return ageString;
+}
